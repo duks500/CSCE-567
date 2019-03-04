a662d5423aebb81e90017ead6f0bdd980c9b591b
Itay Goldfaden <47089377+duks500@users.noreply.github.com>
2019-03-04 17:12:57 -0500

    Update js.js
    style

--- js/js.js
+++ js/js.js
@@ -1,170 +1,91 @@
-var btn2014 = document.getElementById("2014")
-btn2014.addEventListener('click', function(e){
-    change(e.target.id)
+// Define margins, dimensions, and some line colors
+var margin = { left:80, right:20, top:50, bottom:100 };
+var height = 800 - margin.top - margin.bottom,
+    width = 1450 - margin.left - margin.right;
+// Define the scales and tell D3 how to draw the line
+const x = d3.scaleLinear().domain([2008, 2018]).range([0, width]);
+const y = d3.scaleLinear().domain([0, 40000]).range([height, 0]);
+const line = d3.line().x(d => x(d.year)).y(d => y(d.population));
+
+const chart = d3.select('svg').append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+const tooltip = d3.select('#tooltip');
+const tooltipLine = chart.append('line');
+
+// Add the axes and a title
+const xAxis = d3.axisBottom(x).tickFormat(d3.format('.4'));
+const yAxis = d3.axisLeft(y).tickFormat(d3.format('.2s'));
+chart.append('g').call(yAxis);
+chart.append('g').attr('transform', 'translate(0,' + height + ')').call(xAxis);
+chart.append('text').html('Top 6 Fast Food Chains In The USA 2008 - 2018 (According to QSR Magazine) In $MIL').attr('x', 180).style("font-size","30px")
+    .style("font-family","Avant Garde").style("font-weight","700");
+chart.append('text').html("Burger - McDonald's ").attr('x', 40).attr('y', 30).attr("fill", "#FF8C00").style("font-family","Avant Garde").style("font-weight","700");
+chart.append('text').html("Snack - Starbucks Coffe ").attr('x', 40).attr('y', 50).attr("fill", "green").style("font-family","Avant Garde").style("font-weight","700");
+chart.append('text').html("Sandwich - Subway ").attr('x', 40).attr('y', 70).attr("fill", "blue").style("font-family","Avant Garde").style("font-weight","700");
+chart.append('text').html("Mexican - Taco Bell").attr('x', 40).attr('y', 90).attr("fill", "#8B0000").style("font-family","Avant Garde").style("font-weight","700");
+chart.append('text').html("Pizza/Pasta - Pizza Hut").attr('x', 40).attr('y', 110).attr("fill", "grey").style("font-family","Avant Garde").style("font-weight","700");
+chart.append('text').html("Chicken - Chick-fil-A").attr('x', 40).attr('y', 130).attr("fill", "#9932CC").style("font-family","Avant Garde").style("font-weight","700");
+
+
+// Load the data and draw a chart
+let states, tipBox;
+d3.json('data/chain.json', d => {
+    states = d;
+
+    chart.selectAll()
+        .data(states).enter()
+        .append('path')
+        .attr('fill', 'none')
+        .attr('stroke', d => d.color)
+        .attr('stroke-width', 4)
+        .datum(d => d.history)
+        .attr('d', line);
+
+    chart.selectAll()
+        .data(states).enter()
+        .append('text')
+        //name on the side
+        //.html(d => d.name)
+        .attr('fill', d => d.color)
+        .attr('alignment-baseline', 'middle')
+        .attr('x', width+7)
+        .attr('dx', '.5em')
+        .attr('y', d => y(d.currentProfit));
+
+    tipBox = chart.append('rect')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('opacity', 0)
+        .on('mousemove', drawTooltip)
+        .on('mouseout', removeTooltip);
 })
-var btn2002 = document.getElementById("2002")
-btn2002.addEventListener('click', function(e){
-    change(e.target.id)
-})
-var bothData = [
-    {
-        "year": "2014",
-        "product": "Books & DVDs",
-        "purchase": "0.5"
-    },
-    {
-        "year": "2002",
-        "product": "Books & DVDs",
-        "purchase": "10"
-    },
-    {
-        "year": "2014",
-        "product": "Beer & Wine",
-        "purchase": "7"
-    },
-    {
-        "year": "2002",
-        "product": "Beer & Wine",
-        "purchase": "3"
-    },
-    {
-        "year": "2014",
-        "product": "Food",
-        "purchase": "12"
-    },
-    {
-        "year": "2002",
-        "product": "Food",
-        "purchase": "12"
-    },
-    {
-        "year": "2014",
-        "product": "Home Supplies",
-        "purchase": "7"
-    },
-    {
-        "year": "2002",
-        "product": "Home Supplies",
-        "purchase": "6"
-    }
-];
 
-var data2002 = [];
-var data2014 = [];
-
-for(var i = 0; i < bothData.length; i++){
-    if(bothData[i]["year"] === "2002"){
-        data2002.push(bothData[i]);
-    }else{
-        data2014.push(bothData[i]);
-    }
+function removeTooltip() {
+    if (tooltip) tooltip.style('display', 'none');
+    if (tooltipLine) tooltipLine.attr('stroke', 'none');
 }
 
-function change(value){
-    if(value === '2002'){
-        update(data2002);
-    }else if(value === '2014'){
-        update(data2014);
-    }
+function drawTooltip() {
+    const year = Math.floor((x.invert(d3.mouse(tipBox.node())[0]) + 0));// / 10) * 10;
+
+    states.sort((a, b) => {
+        return b.history.find(h => h.year == year).population - a.history.find(h => h.year == year).population;
+    })
+
+    tooltipLine.attr('stroke', 'grey')
+        .attr('x1', x(year))
+        .attr('x2', x(year))
+        .attr('y1', 0)
+        .attr('y2', height);
+
+    tooltip.html(year)
+        .style('display', 'block')
+        .style('left', d3.event.pageX + 20)
+        .style('top', d3.event.pageY - 20)
+        .selectAll()
+        .data(states).enter()
+        .append('div')
+        .style('color', d => d.color)
+        .html(d => d.name + ': ' + d.history.find(h => h.year == year).population + "$").style("font-weight","700");
 }
-
-function update(data){
-    xChart.domain(data.map(function(d){ return d.product; }) );
-    yChart.domain( [0, d3.max(data, function(d){ return + d.purchase; })] );
-
-    var barWidth = width / data.length;
-
-    var bars = chart.selectAll(".bar")
-        .data(data, function(d){ return d.product; })
-    bars.exit()
-        .remove()
-    bars.enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d, i){return i * barWidth + 1 })
-        .attr("y",function(d){ return yChart(d.purchase); })
-        .attr("height",function(d){ return height - yChart(d.purchase); })
-        .attr("width", barWidth - 5)
-        .attr('fill', function(d){
-            if(d.year === "2014"){
-                return'#ea5454'
-            }else{
-                return'#4e97c4'
-            }
-
-        })
-
-
-    bars.transition()
-        .duration(600)
-        .ease(d3.easeLinear)
-        .attr('y', function(d){ return yChart(d.purchase); })
-        .attr('height', function(d){ return height - yChart(d.purchase); })
-        .style('fill', function(d){
-            if(d.year === "2014"){
-                return '#ea5454'
-            } else {
-                return '#4e97c4'
-            }
-        })
-    chart.select('.y').call(yAxis);
-
-
-    chart.select('.xAxis')
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", ".15em")
-        .attr("transform", function(d){
-            return "rotate(-65)";
-        });
-
-}
-
-var margin = {top: 20, right: 20, bottom: 95, left: 50};
-var width = 400;
-var height = 500;
-
-var chart = d3.select(".chart")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var xChart = d3.scaleBand()
-    .range([0, width]);
-
-var yChart = d3.scaleLinear()
-    .range([height, 0]);
-
-var xAxis = d3.axisBottom(xChart);
-var yAxis = d3.axisLeft(yChart);
-
-
-chart.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-
-chart.append("g")
-    .attr("class", "xAxis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis)
-    .selectAll("text")
-    .style("text-anchor", "end")
-    .attr("dx", "-.8em")
-    .attr("dy", ".15em")
-    .attr("transform", function(d){
-        return "rotate(-65)";
-    });
-
-chart.append("text")
-    .attr("transform", "translate(-35," +  (height+margin.bottom)/2 + ") rotate(-90)")
-    .text("Purchases");
-
-chart.append("text")
-    .attr("transform", "translate(" + (width/2) + "," + (height + margin.bottom - 5) + ")")
-    .text("Products");
-
-update(data2002);
